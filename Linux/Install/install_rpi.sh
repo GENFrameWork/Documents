5cb10ddbcf41ee7eae5f14e5fff99ad979afdc13
Sir Graham <sirgraham@endorasoft.com>
2023-01-28 13:11:56 +0100

    Change install_rpi.sh (new sysroot), eliminate script install_rpi64 (It is the same as the install_arm64)

--- Linux/Install/install_rpi.sh
+++ Linux/Install/install_rpi.sh
@@ -2,18 +2,18 @@
 #
 apt-get update
 apt-get install wget rsync -y 
-echo "-----------------------------------------------------------------------"
-cd /root
-wget https://github.com/raspberrypi/tools/archive/master.tar.gz
-mkdir raspberrypi
-tar xzf master.tar.gz -C raspberrypi
-cd raspberrypi
-mv tools-master tools
-# mkdir /usr/local/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/ -p
-cp tools/ /usr/local/rpi -R
-mkdir -p /usr/rpi/rootfs/opt
-echo "-----------------------------------------------------------------------"
-rsync -avz --copy-unsafe-links -e 'ssh' root@192.168.0.20:/lib     /usr/rpi/rootfs/ 
-rsync -avz --copy-unsafe-links -e 'ssh' root@192.168.0.20:/usr     /usr/rpi/rootfs/ 
-rsync -avz --copy-unsafe-links -e 'ssh' root@192.168.0.20:/opt/vc  /usr/rpi/rootfs/opt 
-echo "-----------------------------------------------------------------------"
+cd /usr
+rm rpi -R
+mkdir rpi
+cd rpi
+git clone https://github.com/raspberrypi/tools
+mkdir sysroot
+rsync -avz root@192.168.0.21:/lib sysroot
+rsync -avz root@192.168.0.21:/usr/include sysroot/usr
+rsync -avz root@192.168.0.21:/usr/lib sysroot/usr
+wget https://raw.githubusercontent.com/riscv/riscv-poky/master/scripts/sysroot-relativelinks.py
+chmod +x sysroot-relativelinks.py
+./sysroot-relativelinks.py sysroot
+cp /usr/rpi/sysroot/usr/lib/arm-linux-gnueabihf/crt1.o /usr/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/lib
+cp /usr/rpi/sysroot/usr/lib/arm-linux-gnueabihf/crti.o /usr/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/lib
+cp /usr/rpi/sysroot/usr/lib/arm-linux-gnueabihf/crtn.o /usr/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/lib
